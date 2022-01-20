f56daf81b271b0200b109b79c17292edaca154cb
john gallagher <127highstreet@gmail.com>
2022-01-20 14:29:15 -0500

    first exercise

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,18 @@
 // Put your code here
+
+const names =[  "Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+console.log("All Place Names", );
+// console.log(`${names}`)
+for (let name of names) {
+    console.log(`${name}`);
+  }
+  console.log()
+
+  const theNames = names.filter((n) => n.startsWith("The"));
+ 
+  for (let name of theNames) {
+    console.log(`${name}`);
+  }
+ 
