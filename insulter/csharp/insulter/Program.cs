32205e6d474ffd40ec0fb353d1d4ebd0afffa68c
john gallagher <127highstreet@gmail.com>
2022-01-21 00:35:58 -0500

    saving

--- insulter/csharp/insulter/Program.cs
+++ insulter/csharp/insulter/Program.cs
@@ -10,15 +10,15 @@ namespace Insulter
             
             List <string> insults = new List<string> () 
             {
-                "You look like what morning breath smells like.",
-                "If you tried to give me cpr I would probably throw myself back under water",
-                "I am not a fan of you",
-                "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
-                "I'd rather walk than be on the same plane as you",
-                "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-                "Do you have to be so...like that?"
+                "1, You look like what morning breath smells like.",
+                "2, If you tried to give me cpr I would probably throw myself back under water",
+                "3,I am not a fan of you",
+                "4, You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+                "5, I'd rather walk than be on the same plane as you",
+                "6, If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+                "7, Do you have to be so...like that?"
             };
-            Console.WriteLine("Hello, World!", $"{insults}");
+
 
             int [] indexes = {} ;
 
@@ -31,14 +31,15 @@ namespace Insulter
             for (int i=0; i<indexes.Length; i++){
                 int index = indexes[i];
                 Console.WriteLine(insults[i]);
+                Console.WriteLine(indexes[i]);
             }
 
             Random Candidate = new Random();
             int CandidateInt = Candidate.Next();}
 
 
-            public int Length { get; }
-        }
+        //     public int Length { get; }
+         }
 }
 
 
